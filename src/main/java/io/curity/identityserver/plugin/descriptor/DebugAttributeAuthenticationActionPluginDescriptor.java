a221bf53cfae44e3ee50dd71cf7fd1ac3b97688f
Luís Gonçalves <luis.fgoncalv@gmail.com>
2021-01-20 16:03:58 +0000

    Add HAAPI support

--- src/main/java/io/curity/identityserver/plugin/descriptor/DebugAttributeAuthenticationActionPluginDescriptor.java
+++ src/main/java/io/curity/identityserver/plugin/descriptor/DebugAttributeAuthenticationActionPluginDescriptor.java
@@ -19,10 +19,13 @@ package io.curity.identityserver.plugin.descriptor;
 import io.curity.identityserver.plugin.authenticationaction.debug.attribute.DebugAttributeAuthenticationAction;
 import io.curity.identityserver.plugin.authenticationaction.debug.attribute.DebugAttributeAuthenticationActionConfiguration;
 import io.curity.identityserver.plugin.authenticationaction.debug.attribute.DebugAttributeRequestHandler;
+import io.curity.identityserver.plugin.authenticationaction.debug.attribute.representations.IndexRepresentationFunction;
 import se.curity.identityserver.sdk.authenticationaction.AuthenticationAction;
 import se.curity.identityserver.sdk.authenticationaction.completions.ActionCompletionRequestHandler;
+import se.curity.identityserver.sdk.haapi.RepresentationFunction;
 import se.curity.identityserver.sdk.plugin.descriptor.AuthenticationActionPluginDescriptor;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -53,5 +56,11 @@ public class DebugAttributeAuthenticationActionPluginDescriptor
         Map<String, Class<? extends ActionCompletionRequestHandler<?>>> endpoints = new HashMap<>();
         endpoints.put("index", DebugAttributeRequestHandler.class);
         return endpoints;
+    }
+
+    @Override
+    public Map<String, Class<? extends RepresentationFunction>> getRepresentationFunctions()
+    {
+        return Collections.singletonMap("index", IndexRepresentationFunction.class);
     }
 }
